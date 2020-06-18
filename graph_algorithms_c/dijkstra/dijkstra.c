89265187cca8d5063cf77bc1808c997856207502
AF <alexeifinski337@gmail.com>
2020-06-17 17:52:25 -0700

    added comments and renamed a variable for readability purposes in dijkstra.c

--- graph_algorithms_c/dijkstra/dijkstra.c
+++ graph_algorithms_c/dijkstra/dijkstra.c
@@ -38,10 +38,9 @@ void dijkstra(adj_lst_t *a,
   int h_size = 1;
   int vt_size = sizeof(int);
   int wt_size = a->wt_size;
-  int u;
-  int v;
-  void *wt = malloc(wt_size); //edge weight buffer
-  assert(wt != NULL);
+  int u, v;
+  void *wt_buf = malloc(wt_size); //single edge weight buffer
+  assert(wt_buf != NULL);
   bool *in_heap = calloc(a->num_vts, sizeof(bool));
   assert(in_heap != NULL);
   for (int i = 0; i < a->num_vts; i++){
@@ -53,16 +52,16 @@ void dijkstra(adj_lst_t *a,
   in_heap[s] = true;
   prev[s] = s;
   while (h.num_elts > 0){
-    heap_pop(&h, &u, wt);
+    heap_pop(&h, &u, wt_buf); //weight in wt_buf discarded
     in_heap[u] = false;
     for (int i = 0; i < a->vts[u]->num_elts; i++){
       v = *vt_ptr(a, a->vts[u]->elts, i);
-      //u was popped => wt is not infinity
-      add_wt_fn(wt,
+      //u was popped => sum in wt_buf is not infinity
+      add_wt_fn(wt_buf,
 		wt_ptr(a, dist, u),
 		wt_ptr(a, a->wts[u]->elts, i));
-      if (prev[v] < 0 || cmp_wt_fn(wt_ptr(a, dist, v), wt) > 0){
-	memcpy(wt_ptr(a, dist, v), wt, wt_size);
+      if (prev[v] < 0 || cmp_wt_fn(wt_ptr(a, dist, v), wt_buf) > 0){
+	memcpy(wt_ptr(a, dist, v), wt_buf, wt_size);
 	prev[v] = u;
 	if (in_heap[v]){
 	  heap_update(&h, &v, wt_ptr(a, dist, v));
@@ -74,9 +73,9 @@ void dijkstra(adj_lst_t *a,
     }
   }
   heap_free(&h);
-  free(wt);
+  free(wt_buf);
   free(in_heap);
-  wt = NULL;
+  wt_buf = NULL;
   in_heap = NULL;
 }
 
