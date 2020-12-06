94d79a7443d51a6f9cbe34d5d6bf3bcdc370530e
AF <alexeifinski337@gmail.com>
2020-12-05 17:22:52 -0800

    changed the typedef-name and edited comments in dphil-deadlock-free-2

--- notes/cs170-notes-examples/05-deadlock/dphil-deadlock-free-2.c
+++ notes/cs170-notes-examples/05-deadlock/dphil-deadlock-free-2.c
@@ -4,8 +4,8 @@
    A solution of the "Dining Philosophers" problem. In the provided
    implementation, a thread waits on the condition that both adjacent
    threads are "thinking" (condition variable for each thread). A
-   deadlock cannot occur. Threads are treated fairly according to the
-   scheduler.
+   deadlock cannot occur. Threads are treated according to the
+   thread system and without an additional ordering scheme.
 
    The functions for creating a thread synchronization state and pickup and
    putdown operations are called from the driver implemented in
@@ -35,14 +35,14 @@ typedef struct{
   bool thinking[MAX_NUM_THREADS];
   pthread_cond_t cond_adj_thinking[MAX_NUM_THREADS];
   pthread_mutex_t lock;
-} status_t;
+} state_t;
 
 /**
    Creates a new state for handling thread synchronization wrt the
    pickup and putdown operations.
 */
 void *state_new(int num_phil){
-  status_t *s = malloc_perror(sizeof(status_t));
+  state_t *s = malloc_perror(sizeof(state_t));
   s->num_phil = num_phil; 
   for (int i = 0; i < s->num_phil; i++){
     s->thinking[i] = true;
@@ -61,7 +61,7 @@ void *state_new(int num_phil){
    Performs a pickup operation.
 */
 void state_pickup(void *state, int id){
-  status_t *s = state;
+  state_t *s = state;
   mutex_lock_perror(&s->lock);
   while (!s->thinking[(id + 1) % s->num_phil] ||
 	 !s->thinking[(id + s->num_phil - 1) % s->num_phil]){
@@ -75,7 +75,7 @@ void state_pickup(void *state, int id){
    Performs a putdown operation.
 */
 void state_putdown(void *state, int id){
-  status_t *s = state;
+  state_t *s = state;
   mutex_lock_perror(&s->lock);
   s->thinking[id] = true;
   cond_signal_perror(&s->cond_adj_thinking[(id + 1) % s->num_phil]);
