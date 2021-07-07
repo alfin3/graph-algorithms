e4fe1f2f682ec7dd5daecfe756ba85d32c4f7172
AF <alexeifinski337@gmail.com>
2021-07-06 17:14:34 -0700

    in ht-divchn, added parameter for min known or expected # elements simultaneously present; added tests

--- data-structures/ht-divchn/ht-divchn.c
+++ data-structures/ht-divchn/ht-divchn.c
@@ -275,7 +275,7 @@ static void ht_grow(ht_divchn_t *ht){
   dll_node_t **prev_key_elts = ht->key_elts;
   dll_node_t **head = NULL, *node = NULL;
   ht->count_ix += C_PARTS_PER_PRIME[ht->group_ix];
-  while ((float)ht->num_elts / ht->count > alpha){
+  while ((float)ht->num_elts / ht->count > ht->alpha){
     ht->count_ix += C_PARTS_PER_PRIME[ht->group_ix];
     if (ht->count_ix == C_PARTS_ACC_COUNTS[ht->group_ix]) ht->group_ix++;
     if (ht->count_ix == C_PRIME_PARTS_COUNT){
