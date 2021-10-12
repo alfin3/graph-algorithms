9f7c8b3cbce5e379ee64a2a3aa4c90151c3200f5
AF <alexeifinski337@gmail.com>
2021-10-11 20:37:06 -0700

    generalized integer width and precision determination in dll-test

--- data-structures/dll/dll-test.c
+++ data-structures/dll/dll-test.c
@@ -23,12 +23,8 @@
    unspecified arguments according to the C_ARGS_DEF array.
 
    The implementation of tests does not use stdint.h and is portable under
-   C89/C90 and C99 with the only requirement that the number of value 
-   bits (width) of size_t is even *. 
-
-   * currently CHAR_BIT * sizeof(size_t) and CHAR_BIT * sizeof(int) are used
-     to get bit widths under the assumption that all bits participate in
-     the value (and sign).
+   C89/C90 and C99 with the only requirement that width of size_t is even
+   and less than 2040, and the precision of int is less than 2040. 
 */
 
 #include <stdio.h>
@@ -39,17 +35,18 @@
 #include "dll.h"
 #include "utilities-mem.h"
 #include "utilities-mod.h"
+#include "utilities-lim.h"
 
 /* input handling */
 const char *C_USAGE =
   "dll-test\n"
-  "[0, bit width of int - 2) : i s.t. # inserts = 2**i\n"
+  "[0, precision of int - 2) : i s.t. # inserts = 2**i\n"
   "[0, 1] : on/off prepend append free int test\n"
   "[0, 1] : on/off prepend append free int_ptr (noncontiguous) test\n"
   "[0, 1] : on/off corner cases test\n";
 const int C_ARGC_MAX = 5;
 const size_t C_ARGS_DEF[4] = {13, 1, 1, 1};
-const size_t C_INT_BIT = CHAR_BIT * sizeof(int);
+const size_t C_INT_BIT = UINT_WIDTH_FROM_MAX(INT_MAX);
 
 /* tests */
 const int C_START_VAL = 0;
