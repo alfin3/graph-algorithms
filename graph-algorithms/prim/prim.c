a294bdc8da7b438d6d373a63f4ab8f1237514440
AF <alexeifinski337@gmail.com>
2021-11-10 13:45:03 -0800

    added prim with generic vertices, generic weights, and a hash table parameter

--- graph-algorithms/prim/prim.c
+++ graph-algorithms/prim/prim.c
@@ -1,15 +1,8 @@
 /**
    prim.c
 
-   Prim's algorithm on undirected graphs with generic weights and a hash
-   table parameter.
-
-   If there are vertices outside the connected component of start, an mst of 
-   the connected component of start is computed.
-
-   Vertices are indexed from 0. Edge weights may include negative weights,
-   and are of any basic type (e.g. char, int, long, float, double), or are
-   custom weights within a contiguous block.
+   Prim's algorithm on undirected graphs with generic integer vertices,
+   generic weights (incl. negative) and a hash table parameter.
 
    The hash table parameter specifies a hash table used for in-heap
    operations, and enables the optimization of space and time resources
@@ -22,6 +15,32 @@
    If E >> V, a default hash table may provide speed advantages by avoiding
    the computation of hash values. If V is large and the graph is sparse,
    a non-default hash table may provide space advantages.
+
+   The effective type of every element in the prev array is of the integer
+   type used to represent vertices. The value of every element is set
+   by the algorithm to the value of the previous vertex. If the block
+   pointed to by prev has no declared type then the algorithm sets the
+   effective type of every element to the integer type used to represent
+   vertices by writing a value of the type, including a special value
+   for unreached vertices.
+
+   A distance value in the dist array is only set if the corresponding
+   vertex was reached, as indicated by the prev array, in which case it
+   is guaranteed that the distance object representation is not a trap
+   representation. An element corresponding to a not reached vertex, as
+   indicated by the prev array, may be a trap representation. However,
+   if distances are of an integer type and the dist array is allocated with
+   calloc, then for any integer type the representation with all zero
+   bits is 0 integer value under C99 and C11 (6.2.6.2), and it is safe
+   to read such a representation even if the value was not set by the
+   algorithm.
+
+   The implementation only uses integer and pointer operations. Given
+   parameter values within the specified ranges, the implementation
+   provides an error message and an exit is executed if an integer
+   overflow is attempted or an allocation is not completed due to
+   insufficient resources. The behavior outside the specified parameter
+   ranges is undefined.
 
    The implementation does not use stdint.h and is portable under C89/C90
    and C99.
@@ -36,56 +55,76 @@
 #include "stack.h"
 #include "utilities-mem.h"
 
-typedef enum{FALSE, TRUE} boolean_t;
+static const size_t C_HEAP_INIT_COUNT = 1;
 
 typedef struct{
-  size_t count;
-} context_t;
-
-typedef struct{
-  size_t key_size;
-  size_t elt_size;
-  size_t count;
-  boolean_t *key_present;
-  void *elts;
-  void (*free_elt)(void *);
+  size_t absent;
+  size_t *elts;
+  size_t (*read_vt)(const void *);
 } ht_def_t;
 
-static const size_t C_NREACHED = (size_t)-1; /* not reached as index */
+static void ht_def_init(void *ht,
+			size_t num_vts,
+			size_t (*read_vt)(const void *));
+static void ht_def_insert(void *ht, const void *vt, const void *ix);
+static void *ht_def_search(const void *ht, const void *vt);
+static void ht_def_remove(void *ht, const void *vt, void *ix);
+static void ht_def_free(void *ht);
 
-/* default hash table operations */
-static void ht_def_init(ht_def_t *ht,
-			size_t key_size,
-			size_t elt_size,
-			void (*free_elt)(void *),
-			void *context);
-static void ht_def_insert(ht_def_t *ht, const size_t *key,const void *elt);
-static void *ht_def_search(const ht_def_t *ht, const size_t *key);
-static void ht_def_remove(ht_def_t *ht, const size_t *key, void *elt);
-static void ht_def_free(ht_def_t *ht);
-
-/* functions for computing pointers */
-static void *wt_ptr(const void *wts, size_t i, size_t wt_size);
-static void *elt_ptr(const void *elts, size_t i, size_t elt_size);
+static size_t compute_wt_offset(const adj_lst_t *a);
+static void *ptr(const void *block, size_t i, size_t size);
 
 /**
    Computes and copies the edge weights of an mst of the connected component
    of a start vertex to the array pointed to by dist, and the previous
-   vertices to the array pointed to by prev, with the maximal value of size_t
-   in the prev array for unreached vertices.
+   vertices to the array pointed to by prev, with the number of vertices as
+   the special value in the prev array for unreached vertices.
    a           : pointer to an adjacency list with at least one vertex
-   start       : start vertex
-   dist        : pointer to a preallocated array where the count is equal
-                 to the number of vertices, and the size of an array entry
-                 is equal to the size of a weight in the adjacency list
-   prev        : pointer to a preallocated array with a count that is equal
-                 to the number of vertices in the adjacency list
-   hht         : - NULL pointer, if a default hash table is used for
+   start       : start vertex for running the algorithm
+   dist        : pointer to a preallocated array with the count of elements
+                 equal to the number of vertices in the adjacency list; each
+                 element is of size wt_size (wt_size block) that equals to
+                 the size of a weight in the adjacency list; if the block
+                 pointed to by dist has no declared type then prim sets
+                 the effective type of each element corresponding to a
+                 reached vertex to the type of a weight in the adjacency list
+                 by writing a value of the type; if distances are of an 
+                 integer type and the block was allocated with calloc then
+                 under C99 and C11 each element corresponding to an unreached
+                 vertex can be safely read as the integer type and will
+                 represent 0 value
+   prev        : pointer to a preallocated array with the count equal to the
+                 number of vertices in the adjacency list; each element
+                 is of size vt_size (vt_size block) that equals to the
+                 size of the integer type used to represent vertices in the
+                 adjacency list; if the block pointed to by prev has no
+                 declared type then it is guaranteed that prim sets the
+                 effective type of every element to the integer type used to
+                 represent vertices by writing a value of the type
+   zero_wt     : pointer to a block of size wt_size with a zero value of
+                 the type used to represent weights; this value is copied
+                 to the element in dist corresponding to the start vertex
+   pmht        : - NULL pointer, if a default hash table is used for
                  in-heap operations; a default hash table contains an index
                  array with a count that is equal to the number of vertices
                  - a pointer to a set of parameters specifying a hash table
-                 used for in-heap operations; a vertex is a hash key in 
-                 the hash table
+                 used for in-heap operations
+   read_vt     : reads the integer value of the type used to represent
+                 vertices from the vt_size block pointed to by the argument
+                 and returns a size_t value
+   write_vt    : writes the integer value of the second argument to
+                 the vt_size block pointed to by the first argument
+                 as a value of the integer type used to represent vertices
+   at_vt       : returns a pointer to the element in the array pointed to by
+                 the first argument at the index pointed to by the second
+                 argument; the first argument points to the integer type
+                 used to represent vertices and is not dereferenced; the
+                 second argument points to a value of the integer type used
+                 to represent vertices and is dereferenced
+   cmp_vt      : returns 0 iff the element pointed to by the first
+                 argument is equal to the element pointed to by the second
+                 argument; each argument points to a value of the integer
+                 type used to represent vertices
    cmp_wt      : comparison function which returns a negative integer value
                  if the weight value pointed to by the first argument is
                  less than the weight value pointed to by the second, a
@@ -95,136 +134,169 @@ static void *elt_ptr(const void *elts, size_t i, size_t elt_size);
                  weight values are equal
 */
 void prim(const adj_lst_t *a,
-	  size_t start,
-	  void *dist,
-	  size_t *prev,
-	  const heap_ht_t *hht,
-	  int (*cmp_wt)(const void *, const void *)){
-  const char *p = NULL, *p_start = NULL, *p_end = NULL;
-  const char *uv_wt = NULL;
-  size_t wt_size = a->wt_size;
-  size_t vt_size = sizeof(size_t);
-  size_t init_count = 1;
-  size_t u, v;
-  void *u_wt = NULL, *v_wt = NULL;
+	      size_t start,
+	      void *dist,
+	      void *prev,
+	      const void *wt_zero,
+	      const prim_ht_t *pmht,
+	      size_t (*read_vt)(const void *),
+	      void (*write_vt)(void *, size_t),
+	      void *(*at_vt)(const void *, const void *),
+	      int (*cmp_vt)(const void *, const void *),
+	      int (*cmp_wt)(const void *, const void *)){
   ht_def_t ht_def;
-  context_t context;
-  heap_ht_t hht_def;
+  heap_ht_t hht;
   heap_t h;
-  u_wt = malloc_perror(1, wt_size);
-  memset(dist, 0, a->num_vts * wt_size);
-  memset(prev, 0xff, a->num_vts * vt_size); /* initialize to C_NREACHED */
-  if (hht == NULL){
-    context.count = a->num_vts;
-    hht_def.ht = &ht_def;
-    hht_def.context = &context;
-    hht_def.init = (heap_ht_init)ht_def_init;
-    hht_def.insert = (heap_ht_insert)ht_def_insert;
-    hht_def.search = (heap_ht_search)ht_def_search;
-    hht_def.remove = (heap_ht_remove)ht_def_remove;
-    hht_def.free = (heap_ht_free)ht_def_free;
-    heap_init(&h, init_count, wt_size, vt_size, &hht_def, cmp_wt, NULL);
-  }else{
-    heap_init(&h, init_count, wt_size, vt_size, hht, cmp_wt, NULL);
+  void *p = NULL, *p_start = NULL, *p_end = NULL;
+  void *dp = NULL;
+  /* variables in single block for cache-efficiency */
+  void * const vars =
+    malloc_perror(1, add_sz_perror(compute_wt_offset(a),
+				   mul_sz_perror(2, a->wt_size)));
+  void * const u = vars;
+  void * const nr = (char *)u + a->vt_size;
+  void * const du = (char *)u + compute_wt_offset(a);
+  write_vt(u, start);
+  write_vt(nr, a->num_vts);
+  memcpy(du, wt_zero, a->wt_size);
+  memcpy(ptr(dist, read_vt(u), a->wt_size), wt_zero, a->wt_size);
+  p_start = prev;
+  p_end = ptr(prev, a->num_vts, a->vt_size);
+  for (p = p_start; p != p_end; p = (char *)p + a->vt_size){
+    memcpy(p, nr, a->vt_size);
   }
-  heap_push(&h, wt_ptr(dist, start, wt_size), &start);
-  prev[start] = start;
+  memcpy(at_vt(prev, u), u, a->vt_size);
+  if (pmht == NULL){
+    ht_def_init(&ht_def, a->num_vts, read_vt);
+    hht.ht = &ht_def;
+    hht.alpha_n = 0;
+    hht.log_alpha_d = 0;
+    hht.init = NULL;
+    hht.align = NULL;
+    hht.insert = ht_def_insert;
+    hht.search = ht_def_search;
+    hht.remove = ht_def_remove;
+    hht.free = ht_def_free;
+  }else{
+    hht.ht = pmht->ht;
+    hht.alpha_n = pmht->alpha_n;
+    hht.log_alpha_d = pmht->log_alpha_d;
+    hht.init = pmht->init;
+    hht.align = pmht->align;
+    hht.insert = pmht->insert;
+    hht.search = pmht->search;
+    hht.remove = pmht->remove;
+    hht.free = pmht->free;
+  }
+  heap_init(&h, a->wt_size, a->vt_size, C_HEAP_INIT_COUNT, &hht,
+	    cmp_wt, cmp_vt, read_vt, NULL);
+  heap_push(&h, du, u);
   while (h.num_elts > 0){
-    heap_pop(&h, u_wt, &u);
-    p_start = a->vt_wts[u]->elts;
-    p_end = p_start + a->vt_wts[u]->num_elts * a->pair_size;
-    for (p = p_start; p != p_end; p += a->pair_size){
-      v = *(const size_t *)p;
-      v_wt = wt_ptr(dist, v, wt_size);
-      uv_wt = p + a->offset;
-      if (prev[v] == C_NREACHED){
-	memcpy(v_wt, uv_wt, wt_size);
-	heap_push(&h, v_wt, &v);
-	prev[v] = u;
-      }else if (cmp_wt(v_wt, uv_wt) > 0 && /* hashing after && for efficiency */
-		heap_search(&h, &v) != NULL){
-	memcpy(v_wt, uv_wt, wt_size);
-	heap_update(&h, v_wt, &v);
-	prev[v] = u;
+    heap_pop(&h, du, u);
+    p_start = a->vt_wts[read_vt(u)]->elts;
+    p_end = ptr(p_start, a->vt_wts[read_vt(u)]->num_elts, a->pair_size);
+    for (p = p_start; p != p_end; p = (char *)p + a->pair_size){
+      dp = ptr(dist, read_vt(p), a->wt_size);
+      dp_new = (char *)p + a->wt_offset;
+      if (cmp_vt(at_vt(prev, p), nr) == 0){
+	memcpy(dp, dp_new, a->wt_size);
+	memcpy(at_vt(prev, p), u, a->vt_size);
+	heap_push(&h, dp, p);
+      }else if (cmp_wt(dp, dp_new) > 0 && /* hashing after && */
+		heap_search(&h, p) != NULL){
+	/* was not popped and a better edge found */
+	memcpy(dp, dp_new, a->wt_size);
+	memcpy(at_vt(prev, p), u, a->vt_size);
+	heap_update(&h, dp, p);
       }
     }
   }
   heap_free(&h);
-  free(u_wt);
-  u_wt = NULL;
+  free(vars);
+  /* vars cannot be dereferenced after this line */;
 }
+
 
 /**
-   Default hash table operations. In the main algorithm routine, the
-   elt_size block pointed to by elt in heap_push is a vertex (index).
-   Therefore, a vertex is a hash key in the hash table of the heap, as per
-   the specification of the hash table parameter of the heap.
+   Default hash table operations, mapping values of the integer type
+   used to represent vertices to size_t indices for in-heap operations.
 */
 
-static void ht_def_init(ht_def_t *ht,
-			size_t key_size,
-			size_t elt_size,
-			void (*free_elt)(void *),
-			void *context){
-  context_t *c = context;
-  ht->key_size = key_size;
-  ht->elt_size = elt_size;
-  ht->count = c->count;
-  ht->key_present = calloc_perror(c->count, sizeof(boolean_t));
-  ht->elts = malloc_perror(c->count, elt_size);
-  ht->free_elt = free_elt;
+static void ht_def_init(void *ht,
+			size_t num_vts,
+			size_t (*read_vt)(const void *)){
+  size_t i;
+  ht_def_t *ht_def = ht;
+  ht_def->absent = num_vts;
+  ht_def->elts = malloc_perror(num_vts, sizeof(size_t));
+  for (i = 0; i < num_vts; i++){
+    /* <= num_vts elements in the heap; indices are < num_vts */ 
+    ht_def->elts[i] = num_vts;
+  }
+  ht_def->read_vt = read_vt;
 }
 
-static void ht_def_insert(ht_def_t *ht, const size_t *key, const void *elt){
-  ht->key_present[*key] = TRUE;
-  memcpy(elt_ptr(ht->elts, *key, ht->elt_size),
-	 elt,
-	 ht->elt_size);
+static void ht_def_insert(void *ht, const void *vt, const void *ix){
+  ht_def_t *ht_def = ht;
+  ht_def->elts[ht_def->read_vt(vt)] = *(size_t *)ix; 
 }
 
-static void *ht_def_search(const ht_def_t *ht, const size_t *key){
-  if (ht->key_present[*key]){
-    return elt_ptr(ht->elts, *key, ht->elt_size);
+static void *ht_def_search(const void *ht, const void *vt){
+  const ht_def_t *ht_def = ht;
+  const size_t *p = ht_def->elts + ht_def->read_vt(vt);
+  if (*p != ht_def->absent){
+    return (void *)p;
   }else{
     return NULL;
   }
 }
 
-static void ht_def_remove(ht_def_t *ht, const size_t *key, void *elt){
-  ht->key_present[*key] = FALSE;
-  memcpy(elt,
-	 elt_ptr(ht->elts, *key, ht->elt_size),
-	 ht->elt_size);
-}
-
-static void ht_def_free(ht_def_t *ht){
-  size_t i;
-  if (ht->free_elt != NULL){
-    for (i = 0; i < ht->count; i++){
-      if (ht->key_present[i]){
-	ht->free_elt(elt_ptr(ht->elts, i, ht->elt_size));
-      }
-    }
+static void ht_def_remove(void *ht, const void *vt, void *ix){
+  ht_def_t *ht_def = ht;
+  size_t *p = ht_def->elts + ht_def->read_vt(vt);
+  if (*p != ht_def->absent){
+    *(size_t *)ix = *p;
+    *p = ht_def->absent;
   }
-  free(ht->key_present);
-  free(ht->elts);
-  ht->key_present = NULL;
-  ht->elts = NULL;
 }
 
-/** Functions for computing pointers */
+static void ht_def_free(void *ht){
+  ht_def_t *ht_def = ht;
+  free(ht_def->elts);
+  ht_def->elts = NULL;
+}
 
 /**
-   Computes a pointer to an entry in an array of weights.
+   Computes the wt_offset from malloc's pointer in the vars block
+   consisting of two vt_size blocks followed by one wt_size block.
 */
-static void *wt_ptr(const void *wts, size_t i, size_t wt_size){
-  return (void *)((char *)wts + i * wt_size);
+static size_t compute_wt_offset(const adj_lst_t *a){
+  size_t wt_rem;
+  size_t vt_pair_size = mul_sz_perror(2, a->vt_size);
+  if (vt_pair_size <= a->wt_size) return a->wt_size;
+  wt_rem = vt_pair_size % a->wt_size;
+  return add_sz_perror(vt_pair_size, (wt_rem > 0) * (a->wt_size - wt_rem));
 }
- 
+
 /**
-   Computes a pointer to an entry in the array of elements in a default hash
-   table.
+   Computes a pointer to the ith element in the block of elements.
+
+   According to C89 (draft):
+
+   "It is guaranteed, however, that a pointer to an object of a given
+   alignment may be converted to a pointer to an object of the same
+   alignment or a less strict alignment and back again; the result shall
+   compare equal to the original pointer. (An object that has character
+   type has the least strict alignment.)"
+   
+   "A pointer to void may be converted to or from a pointer to any
+   incomplete or object type. A pointer to any incomplete or object type
+   may be converted to a pointer to void and back again; the result shall
+   compare equal to the original pointer."
+
+   "A pointer to void shall have the same representation and alignment 
+   requirements as a pointer to a character type."
 */
-static void *elt_ptr(const void *elts, size_t i, size_t elt_size){
-  return (void *)((char *)elts + i * elt_size);
+static void *ptr(const void *block, size_t i, size_t size){
+  return (void *)((char *)block + i * size);
 }
--- graph-algorithms/prim/prim.h
+++ graph-algorithms/prim/prim.h
@@ -2,14 +2,8 @@
    prim.h
 
    Declarations of accessible functions for running Prim's algorithm on
-   undirected graphs with generic weights and a hash table parameter.
-
-   If there are vertices outside the connected component of start, an mst of 
-   the connected component of start is computed.
-
-   Vertices are indexed from 0. Edge weights may include negative weights,
-   and are of any basic type (e.g. char, int, long, float, double), or are
-   custom weights within a contiguous block.
+   undirected graphs with generic integer vertices, generic weights (incl.
+   negative) and a hash table parameter.
 
    The hash table parameter specifies a hash table used for in-heap
    operations, and enables the optimization of space and time resources
@@ -23,6 +17,32 @@
    the computation of hash values. If V is large and the graph is sparse,
    a non-default hash table may provide space advantages.
 
+   The effective type of every element in the prev array is of the integer
+   type used to represent vertices. The value of every element is set
+   by the algorithm to the value of the previous vertex. If the block
+   pointed to by prev has no declared type then the algorithm sets the
+   effective type of every element to the integer type used to represent
+   vertices by writing a value of the type, including a special value
+   for unreached vertices.
+
+   A distance value in the dist array is only set if the corresponding
+   vertex was reached, as indicated by the prev array, in which case it
+   is guaranteed that the distance object representation is not a trap
+   representation. An element corresponding to a not reached vertex, as
+   indicated by the prev array, may be a trap representation. However,
+   if distances are of an integer type and the dist array is allocated with
+   calloc, then for any integer type the representation with all zero
+   bits is 0 integer value under C99 and C11 (6.2.6.2), and it is safe
+   to read such a representation even if the value was not set by the
+   algorithm.
+
+   The implementation only uses integer and pointer operations. Given
+   parameter values within the specified ranges, the implementation
+   provides an error message and an exit is executed if an integer
+   overflow is attempted or an allocation is not completed due to
+   insufficient resources. The behavior outside the specified parameter
+   ranges is undefined.
+
    The implementation does not use stdint.h and is portable under C89/C90
    and C99.
 */
@@ -32,26 +52,83 @@
 
 #include <stddef.h>
 #include "graph.h"
-#include "heap.h"
+
+/**
+   Prim hash table parameter struct, pointing to the hash table op
+   helpers, pre-defined in each hash table.
+*/
+typedef struct{
+  void *ht;
+  size_t alpha_n;
+  size_t log_alpha_d;
+  void (*init)(void *,
+	       size_t,
+	       size_t,
+	       size_t,
+	       size_t,
+	       size_t,
+	       int (*)(const void *, const void *),
+	       size_t (*)(const void *),
+	       void (*)(void *),
+	       void (*)(void *));
+  void (*align)(void *, size_t);
+  void (*insert)(void *, const void *, const void *);
+  void *(*search)(const void *, const void *);
+  void (*remove)(void *, const void *, void *);
+  void (*free)(void *);
+} prim_ht_t;
 
 /**
    Computes and copies the edge weights of an mst of the connected component
    of a start vertex to the array pointed to by dist, and the previous
-   vertices to the array pointed to by prev, with the maximal value of size_t
-   in the prev array for unreached vertices.
+   vertices to the array pointed to by prev, with the number of vertices as
+   the special value in the prev array for unreached vertices.
    a           : pointer to an adjacency list with at least one vertex
-   start       : start vertex
-   dist        : pointer to a preallocated array where the count is equal
-                 to the number of vertices, and the size of an array entry
-                 is equal to the size of a weight in the adjacency list
-   prev        : pointer to a preallocated array with a count that is equal
-                 to the number of vertices in the adjacency list
-   hht         : - NULL pointer, if a default hash table is used for
+   start       : start vertex for running the algorithm
+   dist        : pointer to a preallocated array with the count of elements
+                 equal to the number of vertices in the adjacency list; each
+                 element is of size wt_size (wt_size block) that equals to
+                 the size of a weight in the adjacency list; if the block
+                 pointed to by dist has no declared type then prim sets
+                 the effective type of each element corresponding to a
+                 reached vertex to the type of a weight in the adjacency list
+                 by writing a value of the type; if distances are of an 
+                 integer type and the block was allocated with calloc then
+                 under C99 and C11 each element corresponding to an unreached
+                 vertex can be safely read as the integer type and will
+                 represent 0 value
+   prev        : pointer to a preallocated array with the count equal to the
+                 number of vertices in the adjacency list; each element
+                 is of size vt_size (vt_size block) that equals to the
+                 size of the integer type used to represent vertices in the
+                 adjacency list; if the block pointed to by prev has no
+                 declared type then it is guaranteed that prim sets the
+                 effective type of every element to the integer type used to
+                 represent vertices by writing a value of the type
+   zero_wt     : pointer to a block of size wt_size with a zero value of
+                 the type used to represent weights; this value is copied
+                 to the element in dist corresponding to the start vertex
+   pmht        : - NULL pointer, if a default hash table is used for
                  in-heap operations; a default hash table contains an index
                  array with a count that is equal to the number of vertices
                  - a pointer to a set of parameters specifying a hash table
-                 used for in-heap operations; a vertex is a hash key in 
-                 the hash table
+                 used for in-heap operations
+   read_vt     : reads the integer value of the type used to represent
+                 vertices from the vt_size block pointed to by the argument
+                 and returns a size_t value
+   write_vt    : writes the integer value of the second argument to
+                 the vt_size block pointed to by the first argument
+                 as a value of the integer type used to represent vertices
+   at_vt       : returns a pointer to the element in the array pointed to by
+                 the first argument at the index pointed to by the second
+                 argument; the first argument points to the integer type
+                 used to represent vertices and is not dereferenced; the
+                 second argument points to a value of the integer type used
+                 to represent vertices and is dereferenced
+   cmp_vt      : returns 0 iff the element pointed to by the first
+                 argument is equal to the element pointed to by the second
+                 argument; each argument points to a value of the integer
+                 type used to represent vertices
    cmp_wt      : comparison function which returns a negative integer value
                  if the weight value pointed to by the first argument is
                  less than the weight value pointed to by the second, a
@@ -61,9 +138,14 @@
                  weight values are equal
 */
 void prim(const adj_lst_t *a,
-	  size_t start,
-	  void *dist,
-	  size_t *prev,
-	  const heap_ht_t *hht,
-	  int (*cmp_wt)(const void *, const void *));
+	      size_t start,
+	      void *dist,
+	      void *prev,
+	      const void *wt_zero,
+	      const prim_ht_t *pmht,
+	      size_t (*read_vt)(const void *),
+	      void (*write_vt)(void *, size_t),
+	      void *(*at_vt)(const void *, const void *),
+	      int (*cmp_vt)(const void *, const void *),
+	      int (*cmp_wt)(const void *, const void *));
 #endif
